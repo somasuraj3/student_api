88cd0f313ef4612fb2e8b765b16b566b97679c82
Suraj Soma <ssoma@videologygroup.com>
2016-06-16 20:23:27 +0530

    Modified Version in Dockerrun.aws.json file

--- routes/student.js
+++ routes/student.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.route('/students')
 .get(function(req,res){
-	res.json({'Name':'Suraj Soma'});
+	res.json({'Name':'Abhishek Badwaik'});
 })  
 
 module.exports = router;
