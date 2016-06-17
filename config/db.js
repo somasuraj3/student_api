6abe0fa9806e26663e67c1b8f807b430c8a0b72b
Suraj Soma <ssoma@videologygroup.com>
2016-06-17 15:35:52 +0530

    Changing DBURL environment varible value after changing VPC of MongoDB

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-var db_url = process.env.DBURL || 'ec2-54-242-156-243.compute-1.amazonaws.com:27017';
+var db_url = process.env.DBURL || 'localhost:27017';
 
 module.exports = {
 	url : 'mongodb://' + db_url + '/jsdemo_student_master'
